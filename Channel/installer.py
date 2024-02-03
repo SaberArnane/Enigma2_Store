7c02f65dbc83b9fce536dcc8563b3cb9014f21cf
saber <137646541+SaberArnane@users.noreply.github.com>
2024-02-03 11:58:36 +0100

    Update installer.py

--- Channel/installer.py
+++ Channel/installer.py
@@ -51,7 +51,7 @@ Y88b  d88P 888  888 888  888 888  888 888  888 Y8b.     888
  "Y8888P"  888  888 "Y888888 888  888 888  888  "Y8888  888
 """, C, end='')
         print("   Install\n".rjust(30))
-        print("Script by MOHAMED_OS"{}\n".format(
+        print("Script by MOHAMED_OS").format(
             B, C, Y, C).rjust(73), end='')
         
         
