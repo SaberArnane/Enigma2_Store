1b632b9a1b22864aa01b5b053a773514a6ff06a8
saber <137646541+SaberArnane@users.noreply.github.com>
2024-02-03 17:52:41 +0100

    Update installer.py

--- Channel/installer.py
+++ Channel/installer.py
@@ -54,9 +54,9 @@ Y88b  d88P 888  888 888  888 888  888 888  888 Y8b.     888
         print("                  ** Script by: MOHAMED_OS **")
         print("============================================================")
     sleep(2)
-     print("Written by {}Saber-Arnane{} {}(͡๏̯͡๏){}\n".format(
-      B, C, Y, C).rjust(73), end='')
-     print((self.date).rjust(41))
+        print("Written by {}Saber-Arnane{} {}(͡๏̯͡๏){}\n".format(
+        B, C, Y, C).rjust(73), end='')
+        print((self.date).rjust(41))
         
     sleep(2)
               
