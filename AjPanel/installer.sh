1d84dea243ed22e2b2818130b2d75aad702e7da1
saber <137646541+SaberArnane@users.noreply.github.com>
2024-02-04 12:36:32 +0100

    Update installer.sh

--- AjPanel/installer.sh
+++ AjPanel/installer.sh
@@ -6,22 +6,11 @@
 # Command: wget https://raw.githubusercontent.com/SaberArnane/Enigma2_Store/main/AjPanel/installer.sh -qO - | /bin/sh
 #
 # ###########################################
-
-# Colors
-Color_Off='\e[0m'
-Red='\e[0;31m'
-Green='\e[0;32m'
-Yellow='\e[0;33m'
-
 ###########################################
 # Configure where we can find things here #
 TMPDIR='/tmp'
-MY_URL='https://raw.githubusercontent.com/SaberArnane/Enigma2_Store/main/AjPanel'
-
-#########################
-#Develop='ZAKARIYA KHA'
-VERSION='9.2'
 PACKAGE='enigma2-plugin-extensions-ajpanel'
+MY_URL='https://raw.githubusercontent.com/SaberArnane/Enigma2_Store/main/AjPanel'
 
 #########################
 if [ -f /etc/opkg/opkg.conf ]; then
@@ -31,6 +20,7 @@ if [ -f /etc/opkg/opkg.conf ]; then
     OPKGINSTAL='opkg install'
     OPKGLIST='opkg list-installed'
     OPKGREMOV='opkg remove --force-depends'
+    VERSION='20231210'
 elif [ -f /etc/apt/apt.conf ]; then
     STATUS='/var/lib/dpkg/status'
     OSTYPE='DreamOS'
@@ -39,21 +29,22 @@ elif [ -f /etc/apt/apt.conf ]; then
     OPKGLIST='apt-get list-installed'
     OPKGREMOV='apt-get purge --auto-remove'
     DPKINSTALL='dpkg -i --force-overwrite'
+    VERSION='1.0-r7'
 fi
 
 #########################
 install() {
-    if grep -qs "Package: $1" "${STATUS}"; then
+    if grep -qs "Package: $1" "$STATUS"; then
         echo
     else
         $OPKG >/dev/null 2>&1
         echo "   >>>>   Need to install $1   <<<<"
         echo
-        if [ "${OSTYPE}" = "Opensource" ]; then
+        if [ "$OSTYPE" = "Opensource" ]; then
             $OPKGINSTAL "$1"
             sleep 1
             clear
-        elif [ "${OSTYPE}" = "DreamOS" ]; then
+        elif [ "$OSTYPE" = "DreamOS" ]; then
             $OPKGINSTAL "$1" -y
             sleep 1
             clear
@@ -75,45 +66,23 @@ else
 fi
 
 #########################
-
-if [ "${OSTYPE}" = "Opensource" ]; then
-    for i in exteplayer3 gstplayer; do
+if [ "$OSTYPE" = "Opensource" ]; then
+    install enigma2-plugin-systemplugins-serviceapp
+elif [ "$OSTYPE" = "DreamOS" ]; then
+    for i in gstreamer1.0-libav python-pytz; do
         install $i
     done
-elif [ "${OSTYPE}" = "DreamOS" ]; then
-    install gstreamer1.0-plugins-base-apps
 fi
 
 #########################
-clear
-echo -e "${Yellow}" "Downloading AjPanel plugin Please Wait ......" "${Color_Off}"
-if [ "${OSTYPE}" = "Opensource" ]; then
+echo "Insallling AjPanel plugin Please Wait ......"
+if [ "$OSTYPE" = "Opensource" ]; then
     wget $MY_URL/${PACKAGE}_"${VERSION}"_all.ipk -qP $TMPDIR
-else
-    wget $MY_URL/${PACKAGE}_"${VERSION}".deb -qP $TMPDIR
-fi
-if [ $? -gt 0 ]; then
-    echo -e "${Red}" "error downloading file, end" "${Color_Off}"
-    exit 1
-else
-    echo -e "${Green}" "File downloaded" "${Color_Off}"
-fi
-
-if [ "${OSTYPE}" = "Opensource" ]; then
     $OPKGINSTAL $TMPDIR/${PACKAGE}_"${VERSION}"_all.ipk
 else
-    $DPKINSTALL $TMPDIR/${PACKAGE}_"${VERSION}".deb
+    wget $MY_URL/${PACKAGE}_"${VERSION}"_all.deb -qP $TMPDIR
+    $DPKINSTALL $TMPDIR/${PACKAGE}_"${VERSION}"_all.deb
     $OPKGINSTAL -f -y
-fi
-
-if [ $? -gt 0 ]; then
-    echo -e "${Red}" "error install plugin AjPanel, end" "${Color_Off}"
-else
-    echo -e "${Green}" "install plugin AjPanel" "${Color_Off}"
-    for dir in python2 python3; do
-        rm -rf "${TMPDIR}"/"${dir:?}"
-        sleep 1
-    done
 fi
 
 #########################
@@ -124,17 +93,16 @@ clear
 echo ""
 echo "***********************************************************************"
 echo "**                                                                    *"
-echo "**                       AjPanel    : $VERSION                             *"
+echo "**                       AjPanel    : $VERSION                        *"
 echo "**                       Uploaded by: Saber_Arnane                      *"
-echo "**                       Develop by : $Develop                    *"
-echo "**  Support    : https://www.tunisia-sat.com/forums/threads/4171372/  *"
+echo "**                       Develop by : Billy2011                       *"
+echo "**  Support    : https://www.tunisia-sat.com/forums/threads/4226416/  *"
 echo "**                                                                    *"
 echo "***********************************************************************"
 echo ""
 
-if [ "${OSTYPE}" = "Opensource" ]; then
-    init 4
-    init 3
+if [ "$OSTYPE" = "Opensource" ]; then
+    killall -9 enigma2
 else
     systemctl restart enigma2
 fi
